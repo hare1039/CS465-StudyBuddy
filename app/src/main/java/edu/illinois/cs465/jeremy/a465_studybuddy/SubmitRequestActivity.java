f14d1a1ca2b0dc215247310e1fdb09ab5a94d62c
hare1039 <hare39393@gmail.com>
2018-11-28 23:39:30 -0600

    Add xml version of Submit request page

---
+++ app/src/main/java/edu/illinois/cs465/jeremy/a465_studybuddy/SubmitRequestActivity.java
@@ -0,0 +1,13 @@
+package edu.illinois.cs465.jeremy.a465_studybuddy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SubmitRequestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_request);
+    }
+}
