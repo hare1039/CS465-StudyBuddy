e97f4ca8929e3c093c581c7eb147ab7145f54b1c
hare1039 <hare39393@gmail.com>
2018-11-28 22:12:57 -0600

    Add people's feed xml version.
    Java version need more work tobe done, but it look ok now.

---
+++ app/src/main/java/edu/illinois/cs465/jeremy/a465_studybuddy/Account.java
@@ -0,0 +1,12 @@
+package edu.illinois.cs465.jeremy.a465_studybuddy;
+
+public class Account {
+    public String request;
+    public String iconPath;
+    public String name;
+    Account(String r, String p, String n) {
+        request  = r;
+        iconPath = p;
+        name     = n;
+    }
+}
